1efe40ec3ea711498414396e1c6db9799ec9930c
YanyanLi1992 <yanyanli@1848ventures.com>
2026-02-06 17:23:07 -0500

    Final

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import store from './store.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// Import Provider from react-redux
+import { Provider } from 'react-redux';
+
+// Import the Redux store
+import store from './store.js';
+
+// Wrap the App component with Provider and pass the store
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
     <App />
-    </Provider>
-  </React.StrictMode>,
-)
+  </Provider>
+);
